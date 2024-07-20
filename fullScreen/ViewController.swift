81a6eab63f9b9aa6292ffae47988343a10a722e4
Park Hwimok <mok1598@marketon.shop>
2024-07-20 21:25:00 +0900

    Initial Commit

---
+++ fullScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fullScreen
+//
+//  Created by 박휘목 on 7/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

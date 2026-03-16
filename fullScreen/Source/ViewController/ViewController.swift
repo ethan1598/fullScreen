bb9865fba6f0d29f73bd83d51017c187265c66a4
Park Hwimok <mogi@critical.co.kr>
2026-03-16 22:07:10 +0900

    URL을 Config.plist로 분리하고 사이트명을 일반화
    - 하드코딩된 URL을 Config.plist에서 읽도록 변경
    - site1/site2 관련 변수명, UI 텍스트를 site1/site2로 변경
    - Config.plist를 .gitignore에 추가, Config.example.plist 템플릿 제공
    - README 업데이트

--- fullScreen/Source/ViewController/ViewController.swift
+++ fullScreen/Source/ViewController/ViewController.swift
@@ -9,70 +9,84 @@ import UIKit
 import RealmSwift
 
 class ViewController: UIViewController, UITextFieldDelegate {
-    
+
     let ad = UIApplication.shared.delegate as! AppDelegate
     let realm = try! Realm()
-    
+
     let domainList = ["site1", "site2"]
     let nvc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "webViewVC")
-    
+
     @IBOutlet weak var recentSite1Link: UILabel!
     @IBOutlet weak var recentSite2Link: UILabel!
-    
-    //    let picker = UIPickerView()
+
     @IBOutlet weak var goSite1: UIButton!
     @IBOutlet weak var goSite2: UIButton!
-    @IBOutlet weak var site1Title: UITextField!
-    
+    @IBOutlet weak var searchTitle: UITextField!
+
+    // Config.plist에서 URL 로드
+    private var site1URL: String {
+        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let dict = NSDictionary(contentsOfFile: path),
+              let url = dict["Site1URL"] as? String else {
+            return ""
+        }
+        return url
+    }
+
+    private var site2URL: String {
+        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let dict = NSDictionary(contentsOfFile: path),
+              let url = dict["Site2URL"] as? String else {
+            return ""
+        }
+        return url
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //        self.configPickerView()
+
         print("realm 개수 :", realm.objects(UrlInfoRealm.self).count)
-        
-        site1Title.delegate = self
-        
+
+        searchTitle.delegate = self
+
         let keyboardToolbar = UIToolbar()
         let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let doneBarButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.doneButtonTapped))
         keyboardToolbar.items = [flexBarButton, doneBarButton]
         keyboardToolbar.sizeToFit()
-//        keyboardToolbar.tintColor = UIColor.systemGray
 
-        site1Title.inputAccessoryView = keyboardToolbar
+        searchTitle.inputAccessoryView = keyboardToolbar
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.hidesBarsOnSwipe = false
-        
-//        print(realm.objects(UrlInfoRealm.self).filter("urlSrl == 'site1'")[0].urlDomain ?? "")
-        
+
         if realm.objects(UrlInfoRealm.self).filter("urlSrl == 'site1'").count != 0 {
             recentSite1Link.text = realm.objects(UrlInfoRealm.self).filter("urlSrl == 'site1'")[0].urlDomain
         } else {
             recentSite1Link.text = "없음"
         }
-        
+
         if realm.objects(UrlInfoRealm.self).filter("urlSrl == 'site2'").count != 0 {
             recentSite2Link.text = realm.objects(UrlInfoRealm.self).filter("urlSrl == 'site2'")[0].urlDomain
         } else {
             recentSite2Link.text = "없음"
         }
-        
-        site1Title.text = ""
+
+        searchTitle.text = ""
     }
-    
+
     @IBAction func goSite1(_ sender: Any) {
         let newDataUrl = UrlInfoRealm()
         let queryResult = realm.objects(UrlInfoRealm.self).filter("urlSrl == 'site1'")
-        
-        ad.goURL = "https://example1.com/콘텐츠?fil=인기"
+
+        ad.goURL = site1URL
 
         if queryResult.count == 0 {
             do {
                 newDataUrl.urlSrl = "site1"
                 newDataUrl.urlDomain = ad.goURL
-                
+
                 try realm.write {
                     realm.add(newDataUrl)
                 }
@@ -81,116 +95,59 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
         } else {
             ad.goURL = queryResult[0].urlDomain ?? ""
-            
-            if site1Title.text != "" {
-                var site1Domain = ""
+
+            if searchTitle.text != "" {
+                var siteDomain = ""
                 if let convertUrl = URL(string: ad.goURL) {
                     if let domain = convertUrl.host {
-                        // domain 변수에는 "www.example.com"이 저장됩니다.
-                        site1Domain = domain
+                        siteDomain = domain
                     }
                 }
-                
-                ad.goURL = "https://\(site1Domain)/\(site1Title.text!.replacingOccurrences(of: " ", with: "-"))"
+
+                ad.goURL = "https://\(siteDomain)/\(searchTitle.text!.replacingOccurrences(of: " ", with: "-"))"
                 print("convert Title, \(ad.goURL)")
             }
         }
-        
+
         self.navigationController?.pushViewController(nvc, animated: true)
     }
-    
+
     @IBAction func goSite2(_ sender: Any) {
         let newDataUrl = UrlInfoRealm()
         let queryResult = realm.objects(UrlInfoRealm.self).filter("urlSrl == 'site2'")
-        
-        ad.goURL = "https://example2.com/"
-        
+
+        ad.goURL = site2URL
+
         if queryResult.count == 0 {
             do {
                 newDataUrl.urlSrl = "site2"
                 newDataUrl.urlDomain = ad.goURL
-                
+
                 try realm.write {
                     realm.add(newDataUrl)
                 }
             } catch {
-                print("site2 Insert Error \(error)")
+                print("Site2 Insert Error \(error)")
             }
         } else {
             ad.goURL = queryResult[0].urlDomain ?? ""
         }
-        
+
         print(ad.goURL.count)
-        
+
         self.navigationController?.pushViewController(nvc, animated: true)
     }
-    
+
 }
 
 extension ViewController {
     @objc func doneButtonTapped() {
-        // 완료 버튼이 눌렸을 때 실행될 코드
-        site1Title.resignFirstResponder() // 키보드 내리기
-        print("Text entered: \(site1Title.text ?? "")")
+        searchTitle.resignFirstResponder()
+        print("Text entered: \(searchTitle.text ?? "")")
     }
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
 }
-
-//extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-//    func configPickerView() {
-//        picker.delegate = self
-//        picker.dataSource = self
-//        selectDomain.inputView = picker
-//
-//        configToolbar()
-//    }
-//
-//    func configToolbar() {
-//        let toolBar = UIToolbar()
-//        toolBar.barStyle = UIBarStyle.default
-//        toolBar.isTranslucent = true
-//        //        toolBar.tintColor = UIColor.white
-//        toolBar.sizeToFit()
-//
-//        let doneBT = UIBarButtonItem(title: "완료", style: .plain, target: self, action: #selector(self.donePicker))
-//        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-//        let cancelBT = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(self.cancelPicker))
-//
-//        toolBar.setItems([cancelBT,flexibleSpace,doneBT], animated: false)
-//        toolBar.isUserInteractionEnabled = true
-//
-//        selectDomain.inputAccessoryView = toolBar
-//    }
-//
-//    @objc func donePicker() {
-//        let row = self.picker.selectedRow(inComponent: 0)
-//        self.picker.selectRow(row, inComponent: 0, animated: false)
-//        self.selectDomain.text = self.domainList[row]
-//        self.selectDomain.resignFirstResponder()
-//    }
-//
-//    @objc func cancelPicker() {
-//        self.selectDomain.text = nil
-//        self.selectDomain.resignFirstResponder()
-//    }
-//
-//    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
-//        return 1
-//    }
-//
-//    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        return domainList.count
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        return domainList[row]
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        self.selectDomain.text = self.domainList[row]
-//    }
-//}
--- fullScreen/Source/ViewController/WebViewController.swift
+++ fullScreen/Source/ViewController/WebViewController.swift
@@ -11,89 +11,85 @@ import WebKit
 import RealmSwift
 
 class WebViewController: UIViewController, UIScrollViewDelegate {
-    
+
     let ad = UIApplication.shared.delegate as! AppDelegate
     let realm = try! Realm()
-    
+
     @IBOutlet weak var webView: WKWebView!
     var webV = WKWebView()
-    
-    override func viewDidLoad() {
-//        if #available(iOS 16.4, *) {
-//            webView.isInspectable = true
-//        }
-//        
-//        webView.scrollView.delegate = self
-//        webView.navigationDelegate  = self
-//        webView.uiDelegate = self
-//        
-//        webView.allowsBackForwardNavigationGestures = true
-//        
-//        self.webView.addObserver(self, forKeyPath: "URL", options: .new, context: nil)
+
+    // Config.plist에서 사이트 키워드 로드
+    private var siteKeywords: [(key: String, keyword: String)] {
+        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let dict = NSDictionary(contentsOfFile: path) else {
+            return []
+        }
+        var result: [(key: String, keyword: String)] = []
+        if let key = dict["Site1Key"] as? String {
+            result.append((key: "site1", keyword: key))
+        }
+        if let key = dict["Site2Key"] as? String {
+            result.append((key: "site2", keyword: key))
+        }
+        return result
     }
-    
+
+    override func viewDidLoad() {
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(), completionHandler: {
             (records) -> Void in
             for record in records {
                 WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
-                // remove callback
             }
         })
-        
+
         navigationController?.hidesBarsOnSwipe = true
-        
+
         self.navigationItem.title = "\(ad.goURL)"
-        
+
         let url = URL(string: ad.goURL)
         let req = URLRequest(url: url!)
-        
-//        webView.load(req)
-        
+
         setupWebView()
     }
-    
-//    override func viewDidDisappear(_ animated: Bool) {
-//        webView?.loadHTMLString("<html><body></body></html>", baseURL: nil)
-//    }
-    
+
     override var prefersStatusBarHidden: Bool {
         return navigationController?.isNavigationBarHidden ?? false
     }
-    
+
     override var prefersHomeIndicatorAutoHidden: Bool {
         return true
     }
 }
 
 extension WebViewController: WKUIDelegate, WKNavigationDelegate {
-    // WKUserScript가 모든 광고를 즉시 제거하므로 추가 네비게이션 델리게이트 불필요
 }
 
 extension WebViewController {
-    
+
     func setupWebView() {
         let webConfiguration = WKWebViewConfiguration()
         webConfiguration.allowsInlineMediaPlayback = false
-        
+
         // MARK: - 즉시 광고 제거 스크립트 (WKUserScript)
         setupImmediateAdBlocking(webConfiguration: webConfiguration)
-        
+
         webV = WKWebView(frame: .zero, configuration: webConfiguration)
-        
+
         view.addSubview(webV)
-        
+
         webV.scrollView.delegate = self
         webV.navigationDelegate  = self
         webV.uiDelegate = self
-        
+
         webV.allowsBackForwardNavigationGestures = true
-        
-        // URL 로드
+
         let url = URL(string: ad.goURL)!
         let request = URLRequest(url: url)
         webV.load(request)
-        
+
         webV.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             webV.topAnchor.constraint(equalTo: view.topAnchor),
@@ -102,18 +98,17 @@ extension WebViewController {
             webV.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         print("SET UP WEBVIEW")
-        
+
         if #available(iOS 16.4, *) {
             webV.isInspectable = true
         }
-        
+
         webV.addObserver(self, forKeyPath: "URL", options: .new, context: nil)
     }
-    
+
     func extractDomainUrl(urlString: String) -> String {
         if let url = URL(string: urlString) {
             if let domain = url.host {
-                // domain 변수에는 "www.example.com"이 저장됩니다.
                 return domain
             } else {
                 print("No domain found")
@@ -124,50 +119,35 @@ extension WebViewController {
             return ""
         }
     }
-    
-    
+
+
     // MARK: - 즉시 광고 제거 시스템 (WKUserScript)
     func setupImmediateAdBlocking(webConfiguration: WKWebViewConfiguration) {
         let adBlockScript = """
         (function() {
-            console.log('🚀 즉시 광고 차단 스크립트 시작');
-            
-            // 불법 도박 사이트 링크 제거 함수
-            function removeGamblingAds() {
-                // linkbn.php 패턴의 링크들 제거
-                const gamblingLinks = document.querySelectorAll('a[href*="linkbn.php"]');
-                if (gamblingLinks.length > 0) {
-                    console.log('🎰 불법 도박 링크 제거:', gamblingLinks.length + '개');
-                    gamblingLinks.forEach(link => {
-                        link.style.display = 'none';
-                        link.remove();
-                    });
-                }
-                
-                // 기존 광고 요소들 제거
+            function removeAds() {
+                const adLinks = document.querySelectorAll('a[href*="linkbn.php"]');
+                adLinks.forEach(link => {
+                    link.style.display = 'none';
+                    link.remove();
+                });
+
                 const adSelectors = [
                     '.navbar', '#mobile_nav', '.col-md-12.mobile-banner',
                     '.clearfix', '.bn.bnt', '.visible-xs', '#banner_21_img',
                     '#main-banner-view', '#id_mbv', '#hd_pop', '.basic-banner',
                     '.m-list', '.at-go', '.col-md-12.mobile-banner'
                 ];
-                
-                let removedCount = 0;
+
                 adSelectors.forEach(selector => {
                     const elements = document.querySelectorAll(selector);
                     elements.forEach(el => {
                         el.style.display = 'none';
                         el.remove();
-                        removedCount++;
                     });
                 });
-                
-                if (removedCount > 0) {
-                    console.log('🧹 광고 요소 제거:', removedCount + '개');
-                }
             }
-            
-            // DOM 변경 감지 및 실시간 광고 제거
+
             const observer = new MutationObserver(function(mutations) {
                 let shouldCheck = false;
                 mutations.forEach(function(mutation) {
@@ -175,76 +155,71 @@ extension WebViewController {
                         shouldCheck = true;
                     }
                 });
-                
+
                 if (shouldCheck) {
-                    removeGamblingAds();
+                    removeAds();
                 }
             });
-            
-            // DOM이 준비되면 즉시 실행
+
             if (document.readyState === 'loading') {
-                document.addEventListener('DOMContentLoaded', removeGamblingAds);
+                document.addEventListener('DOMContentLoaded', removeAds);
             } else {
-                removeGamblingAds();
+                removeAds();
             }
-            
-            // body가 준비되면 observer 시작
+
             if (document.body) {
-                observer.observe(document.body, { 
-                    childList: true, 
-                    subtree: true 
+                observer.observe(document.body, {
+                    childList: true,
+                    subtree: true
                 });
             } else {
                 document.addEventListener('DOMContentLoaded', function() {
-                    observer.observe(document.body, { 
-                        childList: true, 
-                        subtree: true 
+                    observer.observe(document.body, {
+                        childList: true,
+                        subtree: true
                     });
                 });
             }
-            
-            console.log('✅ 즉시 광고 차단 시스템 활성화');
         })();
         """
-        
+
         let userScript = WKUserScript(
             source: adBlockScript,
-            injectionTime: .atDocumentStart, // 페이지 로드 시작 시점
+            injectionTime: .atDocumentStart,
             forMainFrameOnly: false
         )
-        
+
         webConfiguration.userContentController.addUserScript(userScript)
-        print("🚀 즉시 광고 차단 스크립트 주입 완료")
     }
-    
+
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == #keyPath(WKWebView.url) {
             guard let url = self.webV.url?.absoluteString else {
                 return
             }
-            
+
             var removePercentUrl = url.removingPercentEncoding ?? ""
-            
+
             print("now url = \(removePercentUrl)", url.count)
             self.navigationItem.title = "\(removePercentUrl)"
-            
+
             let checkAdUrl = extractDomainUrl(urlString: ad.goURL)
             let changeUrl = extractDomainUrl(urlString: url)
-            
-            if removePercentUrl.contains("site1") && removePercentUrl.contains("콘텐츠") && removePercentUrl.contains("인기") {
-                if checkAdUrl != changeUrl {
+
+            // site1 URL 변경 감지
+            if checkAdUrl != changeUrl && ad.goURL.contains(extractDomainUrl(urlString: ad.goURL)) {
+                let matchedSite = detectSite(url: removePercentUrl)
+
+                if let site = matchedSite {
                     let alert = UIAlertController(title: "Detect Change URL", message: "요청된 url = \(checkAdUrl)\n변경된 url = \(changeUrl)\n변경된 url로 저장하시겠습니까?", preferredStyle: .alert)
-                    
+
                     let success = UIAlertAction(title: "확인", style: .default){ [self] action in
-                        print("확인 버튼이 눌렸습니다.")
-                        
-                        let existingData = self.realm.objects(UrlInfoRealm.self).filter("urlSrl == 'site1'")
-                        print("취소할 데이터 :", existingData)
-                        
+                        let existingData = self.realm.objects(UrlInfoRealm.self).filter("urlSrl == %@", site)
+
                         let newData = UrlInfoRealm()
-                        newData.urlSrl = "site1"
-                        newData.urlDomain = url.removingPercentEncoding
-                        
+                        newData.urlSrl = site
+                        newData.urlDomain = site == "site1" ? url.removingPercentEncoding : url
+
                         do {
                             try realm.write {
                                 realm.delete(existingData)
@@ -254,58 +229,40 @@ extension WebViewController {
                             print("\(error)")
                         }
                     }
-                    
-                    let cancel = UIAlertAction(title: "취소", style: .cancel){ cancel in
-                        print("취소 버튼이 눌렸습니다.")
-                    }
-                    
+
+                    let cancel = UIAlertAction(title: "취소", style: .cancel)
+
                     alert.addAction(cancel)
                     alert.addAction(success)
-                    
+
                     present(alert, animated: true)
                 }
-                
-                // navigationController 기본 뒤로가기 컨트롤러
-                self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
-            } else if url.contains("site2") && url.count <= 23 {
-                if checkAdUrl != changeUrl {
-                    let alert = UIAlertController(title: "Detect Change URL", message: "요청된 url = \(checkAdUrl)\n변경된 url = \(changeUrl)\n변경된 url로 저장하시겠습니까?", preferredStyle: .alert)
-                    
-                    let success = UIAlertAction(title: "확인", style: .default){ [self] action in
-                        print("확인 버튼이 눌렸습니다.")
-                        
-                        let existingData = self.realm.objects(UrlInfoRealm.self).filter("urlSrl == 'site2'")
-                        print("취소할 데이터 :", existingData)
-                        
-                        let newData = UrlInfoRealm()
-                        newData.urlSrl = "site2"
-                        newData.urlDomain = url
-                        
-                        do {
-                            try realm.write {
-                                realm.delete(existingData)
-                                realm.add(newData)
-                            }
-                        } catch {
-                            print("\(error)")
-                        }
-                    }
-                    
-                    let cancel = UIAlertAction(title: "취소", style: .cancel){ cancel in
-                        print("취소 버튼이 눌렸습니다.")
-                    }
-                    
-                    alert.addAction(cancel)
-                    alert.addAction(success)
-                    
-                    present(alert, animated: true)
-                }
-                
+
                 self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
             } else {
                 self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
             }
-            
         }
+    }
+
+    private func detectSite(url: String) -> String? {
+        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let dict = NSDictionary(contentsOfFile: path) else {
+            return nil
+        }
+
+        if let site1URL = dict["Site1URL"] as? String,
+           let site1Domain = URL(string: site1URL)?.host,
+           url.contains(site1Domain.components(separatedBy: ".").first ?? "") {
+            return "site1"
+        }
+
+        if let site2URL = dict["Site2URL"] as? String,
+           let site2Domain = URL(string: site2URL)?.host,
+           url.contains(site2Domain.components(separatedBy: ".").first ?? "") {
+            return "site2"
+        }
+
+        return nil
     }
 }
